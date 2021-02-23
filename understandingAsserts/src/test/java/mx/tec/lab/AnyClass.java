2c7dfe183d40484f7177b32be89713301caf5358
ATEMarco <49078477+Dracoatloc@users.noreply.github.com>
2021-02-23 10:48:21 -0700

    Primer commit del laboratorio

---
+++ understandingAsserts/src/test/java/mx/tec/lab/AnyClass.java
@@ -0,0 +1,9 @@
+package mx.tec.lab;
+
+public class AnyClass {
+	public void methodThatShouldThrowException() {
+		//5)
+		throw new UnsupportedOperationException("Operation Is Not Supported");
+	}
+
+}
